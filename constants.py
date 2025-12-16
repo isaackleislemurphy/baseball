86e09d1a7017679c5a78c99f4b0ca7f3cb870cbc
Isaac Kleisle-Murphy <isaackleislemurphy@outlook.com>
2025-12-16 13:36:12 -0800

    add general constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""
+"The one constant throughout all the years, Ray, has been baseball."
+
+Sort of a similar deal here, ubiqitous constants for any/all baseball code.
+
+"""
+
+# this guy seems pretty good?
+SHOHEI_OHTANI = 660271
