9098fa7f36f9577e6fb1fa30e3cd6ab1e65b3ced
Isaac Kleisle-Murphy <isaackleislemurphy@outlook.com>
2026-03-25 22:23:56 -0700

    strat constants

--- projects/strategery/constants.py
+++ projects/strategery/constants.py
@@ -35,5 +35,5 @@ GAME_STATES = [
 RUNS_ARRAY = np.arange(MAX_RUNS_PER_INNING + 1)
 
 DB_PATH = os.path.join(os.environ.get("PYTHONPATH"), "baseball", "duckdb", "strategery")
-RE24_PATH = os.path.join(DB_PATH, "re24")
-PE288_PATH = os.path.join(DB_PATH, "pe288")
+RE24_PARQUET_PATH = os.path.join(DB_PATH, "re24")
+PE288_PARQUET_PATH = os.path.join(DB_PATH, "pe288")
