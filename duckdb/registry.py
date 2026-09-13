a7820846aea6899278f4ba971f541357326173c2
Isaac Kleisle-Murphy <isaackleislemurphy@outlook.com>
2026-09-13 08:52:06 -0700

    table config updates

---
+++ duckdb/registry.py
@@ -0,0 +1,7 @@
+"""All active tables should have a `.yaml` in `table_config`; such tables
+enumerated here will be 'picked up'"""
+
+REGISTRY = [
+    "table_config/chadwick__ids.yaml",
+    "table_config/pitch__savant.yaml"
+]
