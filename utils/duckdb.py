af53cc827f7b88dc84d78fe10d849d4e115126ac
Isaac Kleisle-Murphy <isaackleislemurphy@outlook.com>
2026-09-13 07:46:33 -0700

    format

--- utils/duckdb.py
+++ utils/duckdb.py
@@ -7,16 +7,16 @@ import pandas as pd
 def query(sql: str) -> pd.DataFrame:
     """
     Helper to query from DuckDB
-    
+
     Parameters
     ----------
-    sql : str 
+    sql : str
         SQL-like code to hit DuckDB
-    
+
     Returns
     -------
-    pd.DataFrame 
-        The dataframe queried via duckdb. 
+    pd.DataFrame
+        The dataframe queried via duckdb.
     """
     con = duckdb.connect()
     df = con.execute(sql).df()
