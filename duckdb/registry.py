22a37988e99fffae7d2ff4a59e6dfd327c954159
Isaac Kleisle-Murphy <isaackleislemurphy@outlook.com>
2026-09-14 10:54:43 -0700

    win prob sims

--- duckdb/registry.py
+++ duckdb/registry.py
@@ -6,5 +6,5 @@ REGISTRY = [
     "table_config/pitch__savant.yaml",
     "table_config/strategery__re24.yaml",
     "table_config/strategery__re24_transition_probs.yaml",
-    "table_config/strategery__win_probability.yaml",
+    "table_config/strategery__win_probability_simulated.yaml",
 ]
