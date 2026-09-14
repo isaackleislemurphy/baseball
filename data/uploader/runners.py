bf0c523783bf79f2c59954add04d99c98eef9030
Isaac Kleisle-Murphy <isaackleislemurphy@outlook.com>
2026-09-13 22:58:21 -0700

    data updates

--- data/uploader/runners.py
+++ data/uploader/runners.py
@@ -4,6 +4,6 @@ from baseball.data.chadwick.ids import uploader as chadwick
 from baseball.data.pitch.savant import uploader as savant_pitch
 
 RUNNERS = {
-    "chadwick": chadwick.main,
-    "savant_pitch": savant_pitch.main,
+    "chadwick": chadwick.upload,
+    "savant_pitch": savant_pitch.upload,
 }
